f3e2b18fa820d968413a5026bd541dbeeb0208b7
Oscar Zapata <0northbridge.cpu1@gmail.com>
2020-01-28 04:54:28 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,7 +14,9 @@ end
 def age_appropriate_birthday(birthday_kids)
  # "Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nYou are too old for this.\n"
   birthday_kids.each do |kids_name,age|
+    if age <= 12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
+  else 
   puts "You are too old for this."
+end
 end
